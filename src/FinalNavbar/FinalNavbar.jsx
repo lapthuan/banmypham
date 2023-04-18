43bfb1771b5c7d0c8d9c1b84d40b35bfabe5a912
LAPTOP-3NFIFOLQ\Tuan Anh <nguyenhoangtuananh.nt16@gmail.com>
2023-04-18 22:44:08 +0700

    updateNav

--- src/FinalNavbar/FinalNavbar.jsx
+++ src/FinalNavbar/FinalNavbar.jsx
@@ -4,18 +4,20 @@ import Navbars from "../Static-Pages/Navbars/Navbars";
 import Top from "../Static-Pages/Navbardivs/Top";
 import Navbar from "../Static-Pages/Navbardivs/Navbar";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 function FinalNavbar() {
   return (
-    <div style={{
-      zIndex: 999,
-      top: 0,
-      backgroundColor: 'white'
-    }}>
-
+    <div
+      style={{
+        zIndex: 999,
+        top: 0,
+        backgroundColor: "white",
+      }}
+    >
+      <Top />
       <Navbars />
       <MainNavbar />
-      <Top />
+
       <ToastContainer />
       {/* <Navbar/> */}
     </div>
--- src/Static-Pages/Navbardivs/Top.jsx
+++ src/Static-Pages/Navbardivs/Top.jsx
@@ -1,35 +1,37 @@
-import React from 'react'
-import './Top.css'
-import {FaTruck} from 'react-icons/fa'
-import {VscDeviceMobile} from 'react-icons/vsc'
-import {BsBagCheck} from 'react-icons/bs'
-import {FaUserFriends} from 'react-icons/fa'
+import React from "react";
+import "./Top.css";
+import { FaTruck } from "react-icons/fa";
+import { VscDeviceMobile } from "react-icons/vsc";
+import { BsBagCheck } from "react-icons/bs";
+import { FaUserFriends } from "react-icons/fa";
 function Top() {
   return (
     <div>
-         <div className='AdBottom_mainCon'>
-        <div className='AdBottom_innerCon'>
-          <div className='AdBottom_div'>
+      <div className="AdBottom_mainCon">
+        <div className="AdBottom_innerCon">
+          <div className="AdBottom_div">
             <div size="25px" />
-          <FaTruck/><p>FREE US Shipping Over $49</p>
+            <FaTruck />
+            <p>Giao hàng miễn phí</p>
           </div>
-          <div className='AdBottom_div'>
+          <div className="AdBottom_div">
             <div size="25px" />
-          <FaUserFriends/><p>Refer a Friend, Get $15</p>
+            <FaUserFriends />
+            <p>Giới thiệu bạn bè nhận ngay mã giảm</p>
           </div>
-          <div className='AdBottom_div'>
+          <div className="AdBottom_div">
             <div size="25px" />
-          <BsBagCheck/><p>New Customers Save 20% - Use Code NEWBIE</p>
+            <BsBagCheck />
+            <p>Khách hàng giảm 20%</p>
           </div>
-          <div className='AdBottom_div'>
+          <div className="AdBottom_div">
             <div size="25px" />
-            <VscDeviceMobile/> <p>Download Our App</p>
+            <VscDeviceMobile /> <p>Tải ứng dụng qua App</p>
           </div>
         </div>
       </div>
-      
     </div>
-  )
+  );
 }
 
-export default Top
+export default Top;
