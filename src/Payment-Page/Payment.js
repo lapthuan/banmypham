a60ff83c56ba50cfd372a3cce51ca5d99def1ff0
thuan <lapthuan0805@gmail.com>
2023-05-20 14:47:42 +0700

    new

--- src/Payment-Page/Payment.js
+++ src/Payment-Page/Payment.js
@@ -1,149 +1,96 @@
-import React, { useState,useEffect } from "react";
-import { Link ,useNavigate} from "react-router-dom";
+import React, { useState } from "react";
 import "./Payment.css";
-import {BsCheckCircleFill} from 'react-icons/bs'
-function Payment() {
-  const navi=useNavigate()
-  const [data, setdata] = useState([])
-  const [total, settotal] = useState(0)
-  const [totalcost, settotalcost] = useState(0)
-  
-  const [success, setsuccess] = useState(false)
-  const fetchData = async () => {
-    try {
-      let res = await fetch("http://localhost:8080/products");
-      let data = await res.json();
-      console.log(data);
-      // return(data)
-      setdata(data);
-      console.log(data);
+import { TiTick } from "react-icons/ti";
+import { GrContactInfo } from "react-icons/gr"
+import { FaShippingFast } from "react-icons/fa"
+import { MdPayment } from "react-icons/md"
+import { AiOutlineFileDone,AiOutlineProfile } from "react-icons/ai"
 
-const tc=data.reduce((total,a)=>{
-  return total+Number(a.price)
-},0)
-
-
-settotal(tc)
-
-
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-const attheend =()=>{
-setsuccess(true)
-  setTimeout(() => {
-    navi('/')
-// window.alert("helo")
-}, 4000);
-
-}
-
-  if(success){
-    return ( 
-    <>
-<BsCheckCircleFill className="donedanadan"/>
-<h1>Order Placed</h1>
-<p>Thank You for Placing the Order Keep Shoping</p>
-   
-    </>)
-  }
-
-
+const Stepper = () => {
+  const steps = [
+    {
+      name: "Thông tin người nhận",
+      icon: <AiOutlineProfile />,
+    },
+    {
+      name: "Thông tin giao hàng",
+      icon: <FaShippingFast />,
+    },
+    {
+      name: "Thanh toán",
+      icon: <MdPayment />,
+    },
+    {
+      name: "Hoàn thành",
+      icon: <AiOutlineFileDone />,
+    },
+  ];
+  const [currentStep, setCurrentStep] = useState(1);
+  const [complete, setComplete] = useState(false);
   return (
-    <div>
-     <h1>Payment </h1>
-      <nav className="payment_navbar">
-        <Link to="/">
-          <img src="https://s1.thcdn.com/checkout/resources/images/e1ea488cfe9c3b7fc40c08ba4949b1c9.svg" />
-        </Link>
-      </nav>
-
-      <section className="maindiv">
-        <div className="sbhai">
-          <div className="emailwala">
-            <h2 style={{marginBottom: "10px"}}>Checkout as a guest</h2>
-            <p className="uparkro" style={{marginBottom: "5px"}}>
-              Complete your order Without signing up,or sign up after payment
-            </p>
-            <h2 style={{marginBottom: "10px"}}>1.Email and delivery address</h2>
-            <label>Email Address</label>
-            <input type="text" />
-          <p className="uparkro"> We'll send the purchase receipt to this email</p> 
-            <label>Country/Region</label>
-            <input type="text" />
-            <label> Full Name</label>
-            <input type="text" />
-            <label>Zip Code/Postcode</label>
-            <input type="text" name="" id="" />
-            <label>Address</label>
-            <input type="text" />
-            <label>Town/City</label>
-            <input type="text" />
-            <label>Contact</label>
-            <input type="text" />
-          </div>
-          <div className="chotawla">
-            <h2>2.Select Delivery Option</h2>
-            <button>
-              PLease Enter your address so we can calculate your delivery
-              options.
-            </button>
-          </div>
-          <div className="paymentmethod">
-            <h2>3.Select Payment Method</h2>
-
-            <h3>Credit/Debit Card</h3>
-            <div className="chotiimage">
-              <img src="https://s1.thcdn.com/checkout/resources/images/3c2e42cbf9d0b0df0d3b3bb81aa41d6a.svg" alt="" />
-              <img src="https://s1.thcdn.com/checkout/resources/images/57987be4eb98b4c77644d93d92df80fa.svg" alt="" />
-              <img src="https://s1.thcdn.com/checkout/resources/images/6732c0137a7dab50b23daf1337fe2f30.svg" alt="" />
-              <img src="https://s1.thcdn.com/checkout/resources/images/8ba3dc816042268141a2205e3bfc3971.svg" alt="" />
+    <>
+      <div className="flex justify-between">
+        {steps?.map((step, i) => (
+          <div
+            key={i}
+            className={`step-item ${currentStep === i + 1 && "active"} ${(i + 1 < currentStep || complete) && "complete"
+              } `}
+          >
+            <div className="step">
+              {i + 1 < currentStep || complete ? <TiTick size={24} /> : step.icon}
             </div>
-
-            <label>Card Number</label>
-            <input type="password" />
-
-            <label>Name on card</label>
-            <input type="text" />
-
-            <label>Expiry Date</label>
-            <input type="month" />
-<label>Year</label>
-            <input type="year" />
+            <p className="text-gray-500">{step.name}</p>
           </div>
-          <button className="submit" onClick={attheend}>SUBMIT MY ORDER</button>
-        
-        </div>
-         
+        ))}
+      </div>
 
-        
-        
-        <div className="imageswala">
-          {/* wrap in this div  */}
-          <h2>Your products: </h2>
-          {/* {data?.map((el) => (
-            <div className="imgandprice">
-              <div className="onlyimage" key={el.id}>
-                <img src={el.image} alt="nandlal" />
-              </div>
+      {currentStep == 1 ?
+        (
+          <div>
+       
+          </div>
+        ) :
+        currentStep == 2 ?
+          (
+            <div>hello2</div>
+          ) :
+          currentStep == 3 ?
+            (
+              <div>hello3</div>
+            ) :
+            (
+              <div>hello4</div>
+            )
+      }
 
-              <div className="pricedesc">
-                <p>{el.title}</p>
-                <p>Price: {`$${el.price}.00`} </p>
-              </div>
-            </div>
-          ))}
-          <p>Total Price:{total}</p> */}
+      {!complete && (
+        <>
+          {currentStep != 1 ? (
+            <button
+              className="btn"
+              onClick={() => {
+                currentStep === steps.length
+                  ? setComplete(true)
+                  : setCurrentStep((prev) => prev - 1);
+              }}
+            >
+              Trở về
+            </button>) : null}
 
-        </div>
-      </section>
-    </div>
+          <button
+            className="btn"
+            onClick={() => {
+              currentStep === steps.length
+                ? setComplete(true)
+                : setCurrentStep((prev) => prev + 1);
+            }}
+          >
+            {currentStep === steps.length ? "Hoàn thành" : "tiếp tục"}
+          </button>
+        </>
+      )}
+    </>
   );
-}
+};
 
-export default Payment;
+export default Stepper;
